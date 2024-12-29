3eca273c6889ccfc6a65f86a023c6b5cb6d4e2d0
Zatruc <yosanva123@gmail.com>
2024-12-29 17:29:43 -0600

    added the button to restart the game

--- app.js
+++ app.js
@@ -1,64 +1,77 @@
-const gameBoard = document.querySelector('.gameboard');
+const gameBoard = document.querySelector(".gameboard");
 
-const gameInfo = document.querySelector('.info');
+const gameInfo = document.querySelector(".info");
 
-gameInfo.innerHTML = 'Circle is first '
+gameInfo.innerHTML = "Circle is first ";
 
-const gameBoxs = ["","","","","","","","",""];
+const gameBoxs = ["", "", "", "", "", "", "", "", ""];
 
 let start = "circle";
+const restartButton = document.getElementById("restart");
 
-
-
-function gameBox () {
-    gameBoxs.forEach((_cell, index) => {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.id = index;
-        box.addEventListener('click', addGo);
-        gameBoard.append(box);
-    })
+function gameBox() {
+  gameBoxs.forEach((_cell, index) => {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.id = index;
+    box.addEventListener("click", addGo);
+    gameBoard.append(box);
+  });
 }
 
-gameBox()
+gameBox();
 
+function addGo(e) {
+  const goDisay = document.createElement("div");
+  goDisay.classList.add(start);
+  e.target.append(goDisay);
+  start = start === "circle" ? "cross" : "circle";
+  gameInfo.textContent = `it is now ${start}'s go`;
+  e.target.removeEventListener("click", addGo);
 
-function addGo (e) {
-    const goDisay = document.createElement('div');
-    goDisay.classList.add(start);
-    e.target.append(goDisay);
-    start = start === 'circle' ? 'cross' : 'circle';
-    gameInfo.textContent = 'it is now ' + start + "'s go";
-    e.target.removeEventListener("click", addGo);
-
-    checkScore();
+  checkScore();
 }
 
 function checkScore() {
-    const allBox = document.querySelectorAll('.box')
-    const winningCombos = [
-        [0,1,2],[3,4,5],[6,7,8],
-        [0,3,6,],[1,4,7],[2,5,8],
-        [0,4,8],[2,4,6]
-    ]
+  const allBox = document.querySelectorAll(".box");
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-    winningCombos.forEach(array => {
-        const circleWins = array.every(cell => 
-            allBox[cell].firstChild?.classList.contains('circle'))
-        if (circleWins) {
-        gameInfo.textContent = "Circle Wins!";
-        allBox.forEach(box => box.replaceWith(box.cloneNode(true)))
+  winningCombos.forEach((array) => {
+    const circleWins = array.every((cell) =>
+      allBox[cell].firstChild?.classList.contains("circle")
+    );
+    if (circleWins) {
+      gameInfo.textContent = "Circle Wins!";
+      allBox.forEach((box) => box.replaceWith(box.cloneNode(true)));
     }
-    })
+  });
 
-    winningCombos.forEach(array => {
-        const crossWins = array.every(cell => 
-            allBox[cell].firstChild?.classList.contains('cross'))
-        if (crossWins) {
-        gameInfo.textContent = "Cross Wins!";
-        allBox.forEach(box => box.replaceWith(box.cloneNode(true)));
+  winningCombos.forEach((array) => {
+    const crossWins = array.every((cell) =>
+      allBox[cell].firstChild?.classList.contains("cross")
+    );
+    if (crossWins) {
+      gameInfo.textContent = "Cross Wins!";
+      allBox.forEach((box) => box.replaceWith(box.cloneNode(true)));
     }
-    })
-
-    
+  });
 }
+
+const restartGame = () => {
+  start = "circle";
+  gameInfo.textContent = "Circle is first ";
+  gameBoard.innerHTML = "";
+  gameBoxs.fill("");
+  gameBox();
+};
+
+restartButton.addEventListener("click", restartGame);
